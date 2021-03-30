5bf3929641f7b9f2f7a4f41f255ed44f0c162031
Steve Kriz <steve.kriz@gmail.com>
2021-03-30 13:04:30 -0700

    Implement NavBar and routing.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
-import "./App.css";
+import React from "react";
+import { Route, Redirect, Switch } from "react-router-dom";
+import NavBar from "./components/navBar";
+import MovieForm from "./components/movieForm";
 import Movies from "./components/movies";
+import Customers from "./components/customers";
+import Rentals from "./components/rentals";
+import NotFound from "./components/notFound";
+import "./App.css";
 
 function App() {
   return (
-    <main className="container">
-      <Movies />
-    </main>
+    <React.Fragment>
+      <NavBar />
+      <main className="container">
+        <Switch>
+          <Route path="/movies/:id" component={MovieForm} />
+          <Route path="/movies" component={Movies} />
+          <Route path="/customers" component={Customers} />
+          <Route path="/rentals" component={Rentals} />
+          <Route path="/not-found" component={NotFound} />
+          <Redirect from="/" exact to="/movies" />
+          <Redirect to="/not-found" />
+        </Switch>
+      </main>
+    </React.Fragment>
   );
 }
 
