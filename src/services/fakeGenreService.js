3143e6f838bff52f9c6a0c73270909bed2e7888e
Steve Kriz <steve.kriz@gmail.com>
2021-04-06 21:08:07 -0700

    Extract config file and connect movie form to back-end.

--- src/services/fakeGenreService.js
+++
@@ -1,9 +0,0 @@
-export const genres = [
-  { _id: "5b21ca3eeb7f6fbccd471818", name: "Action" },
-  { _id: "5b21ca3eeb7f6fbccd471814", name: "Adventure" },
-  { _id: "5b21ca3eeb7f6fbccd471820", name: "Drama" },
-];
-
-export function getGenres() {
-  return genres.filter((g) => g);
-}
